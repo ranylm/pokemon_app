8138f62a1574f2ce21a9c333df19b56c3ff05218
ranylm <antche423@gmail.com>
2023-04-28 16:28:58 -0400

    Created show views of each pokemon

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/pokemon', (req, res) => {
 })
 
 app.get('/pokemon/:id', (req, res) => {
-  res.send(req.params.id)
+  res.render('Show',{pokemon:pokemon[+req.params.id]})
 })
 
 app.get('/', (req, res) => res.send('Welcome to the Pokemon App!'))
--- views/Show.jsx
+++ views/Show.jsx
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 export default class Show extends Component {
   render() {
-    return <div>Show</div>;
+    const pokemon = this.props.pokemon;
+    return (
+      <div>
+        <h1>Gotta Catch 'Em All</h1>
+        <h2>{pokemon.name}</h2>
+        <img src={`${pokemon.img}.jpg`}></img>
+        <a href="/pokemon">Back</a>
+      </div>
+    );
   }
 }
